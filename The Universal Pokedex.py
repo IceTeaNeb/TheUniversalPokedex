29478b6c3d03b461cdfa17af9b064166c156adfa
IceTeaNeb <nebislife@gmail.com>
2025-10-20 21:39:35 +0100

    updated The Universal Pokedex

--- The Universal Pokedex.py
+++ The Universal Pokedex.py
@@ -18,14 +18,16 @@ class Pokemon():
 
         self.weight = pb.pokemon(id).weight
         self.height = pb.pokemon(id).height
+        self.moves = pb.pokemon(id).moves
 
     def showInfo(self):
         print('Name: ' + self.name)
         print('ID: ' + str(self.id))
         print('Height: ' + str(self.weight))
         print('Weight: ' + str(self.height))
+        print('Moves: ' + ', '.join([move.move.name for move in self.moves]))
 
-class SubMon(Pokemon):
+class SubMon(Pokemon):      #subclass of Pokemon
     def __init__(self, id, name):
         super().__init__(id, name)
         self.type = pb.pokemon(id).types
@@ -33,19 +35,19 @@ class SubMon(Pokemon):
     def showType(self):
         print(self.type)
 
-#def showInfo(pID):
-    #print('Name: ' + pb.pokemon(pID).name)
-    #print('ID: ' + str(pb.pokemon(pID).id))
-    #print('Weight: ' + str(pb.pokemon(pID).weight))
-    #print('Height: ' + str(pb.pokemon(pID).height))
-    #print('Order: ', str(pb.pokemon(pID).order))
+def showInfo(pID):
+    print('Name: ' + pb.pokemon(pID).name)
+    print('ID: ' + str(pb.pokemon(pID).id))
+    print('Weight: ' + str(pb.pokemon(pID).weight))
+    print('Height: ' + str(pb.pokemon(pID).height))
+    print('Order: ', str(pb.pokemon(pID).order))
         
 monName = input("Enter name of Pokémon: ").lower()
 monID = pb.pokemon(monName).id
 #showInfo(monID)
 
 pokemon = SubMon(monID, monName.lower())
-#pokemon.showInfo()
-#pokemon.showType()
-print(pb.pokemon(monID).type)
+pokemon.showInfo()
+pokemon.showType()
+#print(pb.pokemon(monID).types)
 
