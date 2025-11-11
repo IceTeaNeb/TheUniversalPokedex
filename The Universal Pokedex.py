8ad7624b70b30d7036166c13e71b3254841315f0
IceTeaNeb <nebislife@gmail.com>
2025-11-11 11:35:04 +0000

    updated TheUniversalPokedex

--- The Universal Pokedex.py
+++ The Universal Pokedex.py
@@ -16,45 +16,48 @@ from ctypes import windll
 
 
 class Item:
-    def __init__(self, itemGroup, id, gen):
+    def __init__(self, itemGroup, id):
+        validGen = False
         self._itemGroup = itemGroup
         self._id = id
         
-        while validIn == True:
-            if self._itemGroup == 'mon':
-                langDict = {1:1, 2:1, 3:1, 4:1, 5:1, 6:6, 7:7, 8:7, 9:0}   #gen : english index
-                self._gen = pb.pokemon(id).
-                self._name = pb.pokemon(id).name
-                self._flavorText = (pb.pokemon_species.flavor_text_entries[langDict[gen]].flavor_text).replace("\n", " ")
+        if self._itemGroup == 'mon':    #for Mon class
+            langDict = {1:1, 2:1, 3:1, 4:1, 5:1, 6:6, 7:7, 8:7, 9:0}   #gen : english index
+            self._name = pb.pokemon(id).name
+            self._gen = pb.pokemon_species(self._name).generation.id
+            self._flavorText = (pb.pokemon_species.flavor_text_entries[langDict[self._gen]].flavor_text).replace("\n", " ")
 
-            elif self._itemGroup == 'move':
-                langDict = {1:6, 2:6, 3:0, 4:0, 5:1, 6:6, 7:7, 8:7, 9:0}   #gen : english index
-                self._name = pb.move(id).name
-                self._flavorText = (pb.move(id).flavor_text_entries[langDict[gen]].flavor_text).replace("\n", " ")
+        elif self._itemGroup == 'move': #for Move class
+            langDict = {1:6, 2:6, 3:0, 4:0, 5:1, 6:6, 7:7, 8:7, 9:0}   #gen : english index
+            self._name = pb.move(id).name
+            self._gen = pb.move(self._name).generation.id
+            self._flavorText = (pb.move(id).flavor_text_entries[langDict[self._gen]].flavor_text).replace("\n", " ")
 
-            elif self._itemGroup == 'ability':
-                langDict = {3:1, 4:1, 5:1, 6:6, 7:7, 8:7, 9:1}   #gen : english index
-                self._name = pb.ability(id).name
-                self._flavorText = (pb.ability(id).flavor_text_entries[langDict[gen]].flavor_text).replace("\n", " ")
+        elif self._itemGroup == 'ability':  #for Ability class
+            langDict = {3:1, 4:1, 5:1, 6:6, 7:7, 8:7, 9:1}   #gen : english index
+            self._name = pb.ability(id).name
+            self._gen = pb.ability(self._name).generation.id
+            self._flavorText = (pb.ability(id).flavor_text_entries[langDict[self._gen]].flavor_text).replace("\n", " ")
+            
     
     def getItemGroup(self):
         return self._itemGroup
     def getItemName(self):
         return self._name
     def getItemID(self):
-        return self._ID
+        return self._id
     def getItemGen(self):
         return self._gen
     def getFlavorText(self):
         return self._flavorText
 
 class Ability(Item):
-    def __init__(self, itemGroup, id, gen):
-        super().__init__(itemGroup, id, gen)
+    def __init__(self, itemGroup, id):
+        super().__init__(itemGroup, id)
 
 class Move(Item):
-    def __init__(self, itemGroup, id, gen):
-        super().__init__(itemGroup, id, gen)
+    def __init__(self, itemGroup, id):
+        super().__init__(itemGroup, id)
         self._accuracy = pb.move(id).accuracy
         self._effectChance = pb.move(id).accuracy
         self._PP = pb.move(id).pp
@@ -67,10 +70,15 @@ class Move(Item):
         for s in range(len(pb.move(id).stat_changes)):
             self._statChanges[pb.move(id).stat_changes[s].stat] = pb.move(id).stat_changes[s].change
 
+    def getStatChanges(self):
+        return self._statChanges
+
 # class Mon(Item):
-#     def __init__(self, itemGroup, id, gen):
-#         super().__init__(itemGroup, id, gen)
+#     def __init__(self, itemGroup, id):
+#         super().__init__(itemGroup, id)
 #         self._species = pb.genera(id).genus
+
+
 
 # inID = int(input("enter id: "))
 # #print(pb.pokemon(inID).name)
@@ -85,12 +93,18 @@ class Move(Item):
 #print(pb.ability(inID).name)
 #print((pb.ability(inID).flavor_text_entries[1].flavor_text).replace("\n", " "))
 
-
 # def itemInfo():
 #     itemType = input("Enter Item Type: ")
 #     if itemType == 'mon':
 #         myMon = Mon
 
-myMove = Move('move', 370, 4)
-flavorText = myMove.getFlavorText()
+myItem = Move('move', 370)
+name = myItem.getItemName()
+flavorText = myItem.getFlavorText()
+generation = myItem.getItemGen()
+statChanges = myItem.getStatChanges()
+print(name)
 print(flavorText)
+print(generation)
+for i in statChanges:
+    print(i)
